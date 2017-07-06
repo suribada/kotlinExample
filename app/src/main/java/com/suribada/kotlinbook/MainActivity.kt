6701a330ddffa418a966d666fd26f22b8f87f96e
suribada <suribada@korea.com>
2017-07-07 04:36:32 +0900

    apply anko into Activity

--- app/src/main/java/com/suribada/kotlinbook/MainActivity.kt
+++ app/src/main/java/com/suribada/kotlinbook/MainActivity.kt
@@ -2,10 +2,63 @@ package com.suribada.kotlinbook
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MainActivityUi().setContentView(this)
     }
+
+    class MainActivityUi : AnkoComponent<MainActivity> {
+
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                button {
+                    text = "ThreadPool"
+                    onClick {
+                        view -> threadPool()
+                        toast("end ThreadPool! :)") // ui.toast
+                    }
+                }
+
+                button {
+                    text = "coroutine"
+                    onClick {
+                        view -> coroutine()
+                        toast("end Coroutine! :)") // AnkoContext 안에서 가능
+                    }
+                }
+            }
+        }.view()
+
+        fun threadPool() {
+            val threadPool = Executors.newCachedThreadPool()
+            val c = AtomicInteger()
+            for (i in 1..1_000_000) {
+                threadPool.submit { c.addAndGet(i)  }
+            }
+            println(c.get())
+        }
+
+        fun coroutine() {
+            val c = AtomicInteger()
+
+            for (i in 1..1_000_000)
+                launch(CommonPool) {
+                    c.addAndGet(i)
+                }
+
+            println(c.get())
+        }
+    }
+
 }
